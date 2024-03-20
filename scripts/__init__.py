bdcf1b99d3e6ea06f3d890388c5e6fdf6249ba87
TakWolf <takwolf@foxmail.com>
2024-03-20 15:26:40 +0800

    调整缓存目录位置

--- scripts/__init__.py
+++ scripts/__init__.py
@@ -5,6 +5,6 @@ logging.basicConfig(level=logging.DEBUG)
 
 project_root_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 assets_dir = os.path.join(project_root_dir, 'assets')
-cache_dir = os.path.join(project_root_dir, 'cache')
 data_dir = os.path.join(project_root_dir, 'data')
 build_dir = os.path.join(project_root_dir, 'build')
+cache_dir = os.path.join(build_dir, 'cache')
