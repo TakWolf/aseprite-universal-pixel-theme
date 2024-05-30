24d925bdc6de5dc85147b1a50ddae9e0053a9926
TakWolf <takwolf@foxmail.com>
2024-05-30 14:54:57 +0800

    替换网络请求库为 httpx

--- scripts/utils/github_api.py
+++ scripts/utils/github_api.py
@@ -1,10 +1,10 @@
-import requests
+import httpx
 
 _BASE_URL = 'https://api.github.com'
 
 
 def get_releases_latest_tag_name(repository_name: str) -> str:
     url = f'{_BASE_URL}/repos/{repository_name}/releases/latest'
-    response = requests.get(url)
-    assert response.ok, url
+    response = httpx.get(url, follow_redirects=True)
+    assert response.is_success, url
     return response.json()['tag_name']
